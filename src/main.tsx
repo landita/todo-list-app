4b7bdd9de9bb45f8cbc5de8029c79839cd8b2172
nelson <nelson@orbitweb.ca>
2023-03-06 20:27:15 -0600

    improving chunks in the app

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { MantineProvider } from '@mantine/core';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ParticlesLayout } from './components/ui';
 import Home from './pages/home/Home';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
@@ -15,8 +14,6 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         fontFamily: "'Nunito', sans-serif;",
       }}
     >
-      <ParticlesLayout />
-
       <Home />
     </MantineProvider>
   </React.StrictMode>,
